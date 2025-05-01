50e121abef399d2b20ae24e0abedaac3247645b0
Wai_Yan <waiyanbhonemyint2002@gmail.com>
2025-05-01 09:00:59 -0400

    Project about to finish

--- src/main/java/com/ps/Helper.java
+++ src/main/java/com/ps/Helper.java
@@ -24,6 +24,7 @@ public class Helper {
                 space();
                 break;
             case "R":
+                HelperForReport.conditionDependOnUserChoice();
                 break;
             case "H":
                 space();
--- src/main/java/com/ps/HelperForReport.java
+++ src/main/java/com/ps/HelperForReport.java
@@ -1,12 +1,36 @@
 package com.ps;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class HelperForReport {
     static Scanner scanner = new Scanner(System.in);
 
     public static void conditionDependOnUserChoice(){
+        boolean condition = true;
+        while(condition) {
 
+            switch (reportMenuAndUserChoice()) {
+                case 1:
+                    monthToDate();
+                    space();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                default:
+                    System.out.println("Please choose only an available option.");
+            }
+        }
     }
 
     public static Integer reportMenuAndUserChoice(){
@@ -23,6 +47,64 @@ public class HelperForReport {
     }
 
     public static void monthToDate(){
+        System.out.println("Which month of transaction you would like to display? Please enter the number of month: ");
+        int userInputOfMonth = scanner.nextInt();
 
+        System.out.printf("%-12s | %-8s | %-20s | %-15s | %-10s%n",
+                "Date", "Time", "Description", "Vendor", "Amount");
+
+        System.out.println("------------------------------------------------------------------");
+
+        try{
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("transactions.csv"));
+
+            String line;
+            while((line = bufferedReader.readLine()) != null){
+                String[] data = line.split("\\|");
+                LocalDate date = LocalDate.parse(data[0].trim());
+                int month = date.getMonthValue();
+                if(userInputOfMonth == month){
+                    System.out.printf("%-12s | %-8s | %-20s | %-15s | %-10s%n",
+                            data[0], data[1], data[2], data[3], data[4]);
+                }
+            }
+        } catch (Exception e) {
+
+        }
+
+    }
+
+    public static void previousMonth(){
+        System.out.println("Which month of transaction you would like to display? Please enter the number of month: ");
+        int userInputOfMonth = scanner.nextInt();
+
+        System.out.printf("%-12s | %-8s | %-20s | %-15s | %-10s%n",
+                "Date", "Time", "Description", "Vendor", "Amount");
+
+        System.out.println("------------------------------------------------------------------");
+
+        try{
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("transactions.csv"));
+
+            String line;
+            while((line = bufferedReader.readLine()) != null){
+                String[] data = line.split("\\|");
+                LocalDate date = LocalDate.parse(data[0].trim());
+                int month = date.getMonthValue();
+                if(userInputOfMonth == month){
+                    System.out.printf("%-12s | %-8s | %-20s | %-15s | %-10s%n",
+                            data[0], data[1], data[2], data[3], data[4]);
+                }
+            }
+        } catch (Exception e) {
+
+        }
+
+    }
+
+    public static void space() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println();
+        }
     }
 }
